843535580c53decbe1025374afa2dec5d91118c4
tallmansamadam <tallman.samadam@gmail.com>
2023-07-27 11:20:32 -0500

    Added API guide

--- main.py
+++ main.py
@@ -51,7 +51,9 @@ col1, col2 = st.columns(2)
 with col1:
     st.markdown("Often professionals would like to improve their emails, but don't have the skills to do so. \n\n This tool \
                 will help you improve your email skills by converting your emails into a more professional format. This tool \
-                is powered by [LangChain](https://langchain.com/) and [OpenAI](https://openai.com)")
+                is powered by [LangChain](https://langchain.com/) and [OpenAI](https://openai.com). \
+                If you don't have an OpenAI API Key to enter, follow this video guide https://www.youtube.com/watch?v=EQQjdwdVQ-M "
+                )
 
 with col2:
     st.image(image='searchBot.jpg', width=500, caption='Use this tool to juice up your emails')
